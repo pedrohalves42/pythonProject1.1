026580c22f25b45d02dcf92a40ab8f4035fc3168
pedrohalves42@gmail.com <pedrohalves42@gmail.com>
2022-06-06 17:00:30 -0300

    Aprendizado_python

---
+++ modulos.py
@@ -0,0 +1,10 @@
+import sys
+import random
+
+
+def randit(*args):
+    return 'hahahaha'
+
+
+for i in range(10):
+    print(random.randint(0, 15))
---
+++ vendas/cal_preco.py
@@ -0,0 +1,8 @@
+def aumento(valor,porcentagem):
+    r = valor +(valor * (porcentagem /100))
+    return  r
+
+
+def reducao(valor,porcentagem):
+    r = valor - (valor * (porcentagem / 100))
+    return r
