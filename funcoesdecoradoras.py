d9d73ca165c01fd686e5e8f6166e5b0ae7fe32f6
pedrohalves42@gmail.com <pedrohalves42@gmail.com>
2022-06-20 10:02:18 -0300

    Aprendizado_python

--- funcoesdecoradoras.py
+++ funcoesdecoradoras.py
@@ -1,13 +1,43 @@
+'''
 def master(funcao):
-    def slave():
-        print('Oi')
+    def slave(*args,**kwargs):
+        print('Agora estou decorada ')
+        funcao(*args,**kwargs)
 
     return slave
 
-
+@master
 def fala_oi():
     print('Oi')
 
+@master
+def outra_funcao(msg):
+    print(msg)
 
-variavel = master(fala_oi())
-variavel()
+
+fala_oi = master(fala_oi)
+
+fala_oi()
+'''
+from time import time, sleep
+
+
+def velocidade(funcao):
+    def interna(*args, **kwargs):
+        start_time = time()
+        resultado = funcao(*args, **kwargs)
+        end_time = time()
+        tempo = (end_time - start_time) * 1000
+        print(f'a função {funcao.__name__} levou {tempo:.2f}ms para executar ')
+        return resultado
+    return interna()
+
+
+@velocidade
+def demora():
+    for i in range(5):
+        print(i)
+        sleep(0.5)
+
+demora
+
